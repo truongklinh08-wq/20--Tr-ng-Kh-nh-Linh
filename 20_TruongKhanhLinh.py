8a896d38d375c4c94f1957d7e25f645c6c297646
Linh <truongklinh08@gmail.com>
2025-10-21 00:52:18 +0700

    feat: thêm tính năng thêm sinh viên

--- 20_TruongKhanhLinh.py
+++ 20_TruongKhanhLinh.py
@@ -1,17 +1,4 @@
-# student_manager.py
-# Họ tên: Trương Khánh Linh – MSSV: 20
-
-# Danh sách để lưu thông tin các sinh viên.
-# Mỗi sinh viên là một dictionary.
-student_list = []
-
 def add_student(name, year_of_birth, address):
-    """
-    YÊU CẦU 1:
-    - Tạo một dictionary để lưu thông tin sinh viên.
-    - Thêm dictionary đó vào danh sách `student_list`.
-    - In ra thông báo "Da them sinh vien <ten> thanh cong."
-    """
     student = {
         "name": name,
         "year_of_birth": year_of_birth,
@@ -19,3 +6,4 @@ def add_student(name, year_of_birth, address):
     }
     student_list.append(student)
     print(f"Da them sinh vien {name} thanh cong.")
+
